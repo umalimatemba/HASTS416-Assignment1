4bd7f4b44b839d4ee3dd8a12618dfc09441bd9b1
umalimatemba <umalimatemba6@gmail.com>
2026-03-31 19:01:47 +0000

    Create A2Part(c).R

---
+++ A2Part(c).R
@@ -0,0 +1,5 @@
+set.seed(42)
+traj1 <- rmarkovchain(n=50, object=mc2, t0=sample(states2,1))
+traj2 <- rmarkovchain(n=50, object=mc2, t0=sample(states2,1))
+par(mfrow=c(2,1))
+plot(as.numeric(traj1), type="b", col="blue", ylim=c(0,6),

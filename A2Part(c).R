08a0269b9d76adb6e8112e7c9335864da937f5c5
umalimatemba <umalimatemba6@gmail.com>
2026-03-31 19:05:06 +0000

    Update A2Part(c).R

--- A2Part(c).R
+++ A2Part(c).R
@@ -3,3 +3,6 @@ traj1 <- rmarkovchain(n=50, object=mc2, t0=sample(states2,1))
 traj2 <- rmarkovchain(n=50, object=mc2, t0=sample(states2,1))
 par(mfrow=c(2,1))
 plot(as.numeric(traj1), type="b", col="blue", ylim=c(0,6),
+     xlab="Step", ylab="State", main="Trajectory 1")
+plot(as.numeric(traj2), type="b", col="red", ylim=c(0,6),
+     xlab="Step", ylab="State", main="Trajectory 2")
